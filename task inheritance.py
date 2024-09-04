0398161f8b452393d43c894b978216b3b8ca8299
Shemeema <shemeemathasni548@gmail.com>
2024-09-04 22:40:46 +0530

    Add files via upload

---
+++ task inheritance.py
@@ -0,0 +1,25 @@
+class Shape:
+    def area(self):
+        return 0
+
+class square(Shape):
+    def __init__(self, side):
+        super().__init__()
+        self.side=side
+    
+    def area(self):
+        return self.side**2
+    
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+
+s = square(5)
+r= Rectangle(7,9)
+print(s.area())
+print(r.area())
